348ccf1fa27ae05778cad06fe56be66fc8a8647f
Bernard Chan <b.c.chan@student.unsw.edu.au>
2021-07-17 23:38:10 +1000

    added generate link button, atm generated link is the input link

--- imports/ui/components/Teacher/Generate/Generate.jsx
+++ imports/ui/components/Teacher/Generate/Generate.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 function Generate () {
   return (
     <div>
-      This is where we generate the links
+      <div>
+        Your Inputted Link: {localStorage.getItem("inputLink")}
+      </div>
+      <br></br>
+      <div>
+        Preview your video:
+      </div>
+      <div>
+        {localStorage.getItem("generatedLink")}
+      </div>
     </div>
   );
 }
