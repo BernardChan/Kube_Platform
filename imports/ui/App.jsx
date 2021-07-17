2689c2e6d2976c29218ded82e17c3a401a6ad0b5
William Liu <william.liu142@gmail.com>
2021-07-17 17:58:14 +1000

    Hopefully works

---
+++ imports/ui/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+import { Switch, Route, } from 'react-router-dom'
+import Home from './components/Home/Home.jsx';
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <Hello />
+    <Info />
+
+    <Switch>
+      <Route
+        exact path="/Home"
+        component={Home}
+        key="home"
+      />
+    </Switch>
+  </div>
+);
