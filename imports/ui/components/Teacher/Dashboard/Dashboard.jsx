b68090bb5b436b8abdf2331aba119584216b6b85
Bernard Chan <b.c.chan@student.unsw.edu.au>
2021-07-17 22:50:09 +1000

    dashbaord update

--- imports/ui/components/Teacher/Dashboard/Dashboard.jsx
+++ imports/ui/components/Teacher/Dashboard/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
 
 function Dashboard () {
   return (
     <div>
+      <div>
+        <Link to ="/Teacher">
+          <button>Log out</button>
+        </Link>
+      </div>
+      <div>
+      </div>
       This is the teacher's dashboard
     </div>
   );
