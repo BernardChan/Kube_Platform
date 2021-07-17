37cdae35d816d2d59f675684516ffb4e013c0f2e
Pennie <z5201973@student.unsw.edu.au>
2021-07-17 21:36:40 +1000

    basic user methods

---
+++ server/methods/users.js
@@ -0,0 +1,61 @@
+Meteor.methods ({
+
+  // add new user to the database
+  'users/createNewUser': ({
+    email,
+    password,
+    firstName,
+    lastName,
+    userType, // Currently default set to educator
+  }) => {
+
+    check(email, String);
+    check(password, String);
+    check(firstName, String);
+    check(lastName, String);
+    check(userType, String);
+
+    const data = {
+      email: email,
+      password: password, //TODO: add security to password haha
+      firstName: firstName,
+      lastName: lastName,
+      userType: userType,
+    }
+
+    const {
+      error, result,
+    } = Accounts.createUser(data);
+
+    if (error) {
+      throw new Meteor.Error(400, 'Bad Request');
+    }
+    return result
+  },
+
+  // TODO: Update user fields?
+
+  'users/delete': ({userId, password}) => {
+
+    check(email, String);
+    check(password, String);
+
+    const currentUser = Meteor.user();
+    if (Meteor.isDevelopement) console.log(currentUser);
+
+    if ((currentUser.password == password) && (currentUser._id == userId)) {
+
+      Users.update(userId,
+        {
+          $set: {
+            deleted: true}
+        })
+
+    }
+
+    return data;
+    
+  },
+
+})
+
